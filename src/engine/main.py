02d0be4cd642c9a0376602322b35538c4cc16566
vitaliypopel <vitaliypopel14@gmail.com>
2026-05-28 16:53:33 +0300

    Created calculation functionality with basic IO operation

--- src/engine/main.py
+++ src/engine/main.py
@@ -15,9 +15,10 @@ def julian_date(dt: datetime) -> float:
 def gmst(jd: float) -> float:
     T = (jd - 2451545.0) / 36525.0
 
-    gmst_deg = 280.46061837 + 360.98564736629 * (jd - 2451545.0)
-    gmst_deg += 0.000387933 * T**2 - T**3 / 38710000.0
-    gmst_deg %= 360
+    gmst_deg = (
+        280.46061837 + 360.98564736629 * (jd - 2451545.0) +
+        0.000387933 * T**2 - T**3 / 38710000.0
+    ) % 360
 
     return gmst_deg
 
@@ -79,6 +80,20 @@ def decimal_to_sexagesimal(deg: float) -> tuple[int, int, float]:
     return sign * d, m, s
 
 
+def calculate_alt_az(
+    ra_h: float, dec_deg: float, lat_deg: float, lon_deg: float, dt: datetime,
+) -> tuple[tuple[int, int, float], tuple[int, int, float]]:
+    jd = julian_date(dt)
+    gmst_deg = gmst(jd)
+    lst_deg = lst(gmst_deg, lon_deg)
+
+    alt_deg, az_deg = equatorial_to_horizontal(ra_h, dec_deg, lat_deg, lst_deg)
+
+    alt_dms, az_dms = decimal_to_sexagesimal(alt_deg), decimal_to_sexagesimal(az_deg)
+
+    return alt_dms, az_dms
+
+
 def main():
     sirius_ra_h = 6.7525
     sirius_dec_deg = -16.7161
@@ -89,13 +104,7 @@ def main():
     while True:
         dt = datetime.now(timezone.utc)
 
-        jd = julian_date(dt)
-        gmst_deg = gmst(jd)
-        lst_deg = lst(gmst_deg, lon_deg)
-
-        alt_deg, az_deg = equatorial_to_horizontal(sirius_ra_h, sirius_dec_deg, lat_deg, lst_deg)
-
-        alt_dms, az_dms = decimal_to_sexagesimal(alt_deg), decimal_to_sexagesimal(az_deg)
+        alt_dms, az_dms = calculate_alt_az(sirius_ra_h, sirius_dec_deg, lat_deg, lon_deg, dt)
 
         print(
             f'alt: {alt_dms[0]}° {alt_dms[1]}\' {alt_dms[2]}\"\n'
