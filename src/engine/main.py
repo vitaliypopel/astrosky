e5ceb791471fc14a3891e7eb7bc1a1c7a0b1e9a6
vitaliypopel <vitaliypopel14@gmail.com>
2026-05-28 17:21:40 +0300

    Separated utilities functions, added constants and imporved code style

--- src/engine/main.py
+++ src/engine/main.py
@@ -3,60 +3,24 @@ import time
 
 from datetime import datetime, timezone
 
-
-def julian_date(dt: datetime) -> float:
-    timestamp = dt.timestamp()
-
-    jd = timestamp / 86400.0 + 2440587.5
-
-    return jd
+JD_UNIX_EPOCH = 2440587.5
+J2000 = 2451545.0
 
 
-def gmst(jd: float) -> float:
-    T = (jd - 2451545.0) / 36525.0
-
-    gmst_deg = (
-        280.46061837 + 360.98564736629 * (jd - 2451545.0) +
-        0.000387933 * T**2 - T**3 / 38710000.0
-    ) % 360
-
-    return gmst_deg
+def wrap360(deg: float) -> float:
+    return deg % 360.0
 
 
-def lst(gmst_deg: float, lon_deg: float) -> float:
-    lst_deg = (gmst_deg + lon_deg) % 360
-
-    return lst_deg
+def wrap180(deg: float) -> float:
+    return (deg + 180.0) % 360.0 - 180.0
 
 
-def equatorial_to_horizontal(
-    ra_h: float, dec_deg: float, lat_deg: float, lst_deg: float,
-) -> tuple[float, float]:
-    ra_deg = ra_h * 15
+def deg2rad(deg: float) -> float:
+    return math.radians(deg)
 
-    ha_deg = lst_deg - ra_deg
-    ha_deg = (ha_deg + 180) % 360 - 180
 
-    ha, dec, lat = math.radians(ha_deg), math.radians(dec_deg), math.radians(lat_deg)
-
-    sin_alt = (
-        math.sin(dec) * math.sin(lat) +
-        math.cos(dec) * math.cos(lat) * math.cos(ha)
-    )
-
-    alt = math.asin(sin_alt)
-
-    sin_az = -math.sin(ha) * math.cos(dec)
-    cos_az = (
-        math.sin(dec) - math.sin(alt) * math.sin(lat)
-    ) / (math.cos(alt) * math.cos(lat))
-
-    az = math.atan2(sin_az, cos_az)
-
-    alt_deg = math.degrees(alt)
-    az_deg = (math.degrees(az) + 360) % 360
-
-    return alt_deg, az_deg
+def rad2deg(rad: float) -> float:
+    return math.degrees(rad)
 
 
 def sexagesimal_to_decimal(d: int, m: int, s: float) -> float:
@@ -80,8 +44,70 @@ def decimal_to_sexagesimal(deg: float) -> tuple[int, int, float]:
     return sign * d, m, s
 
 
+def julian_date(dt: datetime) -> float:
+    timestamp = dt.timestamp()
+
+    jd = timestamp / 86400.0 + JD_UNIX_EPOCH
+
+    return jd
+
+
+def gmst(jd: float) -> float:
+    T = (jd - J2000) / 36525.0
+
+    gmst_deg = (
+        280.46061837 + 360.98564736629 * (jd - J2000) +
+        0.000387933 * T**2 - T**3 / 38710000.0
+    )
+
+    return wrap360(gmst_deg)
+
+
+def lst(gmst_deg: float, lon_deg: float) -> float:
+    lst_deg = gmst_deg + lon_deg
+
+    return wrap360(lst_deg)
+
+
+def equatorial_to_horizontal(
+    ra_h: float,
+    dec_deg: float,
+    lat_deg: float,
+    lst_deg: float,
+) -> tuple[float, float]:
+    ra_deg = ra_h * 15.0
+
+    H = wrap180(lst_deg - ra_deg)
+
+    ha = deg2rad(H)
+    dec = deg2rad(dec_deg)
+    lat = deg2rad(lat_deg)
+
+    sin_alt = (
+        math.sin(dec) * math.sin(lat) +
+        math.cos(dec) * math.cos(lat) * math.cos(ha)
+    )
+
+    alt = math.asin(sin_alt)
+
+    sin_az = -math.sin(ha) * math.cos(dec)
+    cos_az = (
+        math.sin(dec) - math.sin(alt) * math.sin(lat)
+    ) / (math.cos(alt) * math.cos(lat))
+
+    az = math.atan2(sin_az, cos_az)
+
+    alt_deg, az_deg = rad2deg(alt), wrap360(rad2deg(az))
+
+    return alt_deg, az_deg
+
+
 def calculate_alt_az(
-    ra_h: float, dec_deg: float, lat_deg: float, lon_deg: float, dt: datetime,
+    ra_h: float,
+    dec_deg: float,
+    lat_deg: float,
+    lon_deg: float,
+    dt: datetime,
 ) -> tuple[tuple[int, int, float], tuple[int, int, float]]:
     jd = julian_date(dt)
     gmst_deg = gmst(jd)
@@ -104,7 +130,9 @@ def main():
     while True:
         dt = datetime.now(timezone.utc)
 
-        alt_dms, az_dms = calculate_alt_az(sirius_ra_h, sirius_dec_deg, lat_deg, lon_deg, dt)
+        alt_dms, az_dms = calculate_alt_az(
+            sirius_ra_h, sirius_dec_deg, lat_deg, lon_deg, dt,
+        )
 
         print(
             f'alt: {alt_dms[0]}° {alt_dms[1]}\' {alt_dms[2]}\"\n'
