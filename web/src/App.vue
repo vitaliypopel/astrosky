192b4d597f9f32c904dee6b38236426e3bcfb99d
vitaliypopel <vitaliypopel14@gmail.com>
2026-06-15 18:31:17 +0300

    Created global container component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
+import ContainerLayout from '@/components/ContainerLayout.vue'
 import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
   <NavBar />
-  <RouterView />
+  <ContainerLayout>
+    <RouterView />
+  </ContainerLayout>
 </template>
